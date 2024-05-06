3e7981edaac418fd141fcaf17bdc203a549fdda0
Pawlo Picasso <Quaslusive@protonmail.com>
2024-05-06 23:53:43 +0200

    v0.0.1

--- src/ui/LibraryManagementSystem.java
+++ src/ui/LibraryManagementSystem.java
@@ -66,7 +66,7 @@ public class LibraryManagementSystem extends JFrame {
 
         add(mainPanel);
     }
-
+/*
     private void addBook() {
         String title = titleField.getText();
         String author = authorField.getText();
@@ -86,6 +86,8 @@ public class LibraryManagementSystem extends JFrame {
             JOptionPane.showMessageDialog(null, "Failed to add book.", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
+
+ */
 
     private void searchBook() {
         String searchTerm = searchField.getText().trim();
